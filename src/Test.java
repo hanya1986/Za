710c9642eb4747fb69f6b98f3eabe35a15d9eb6f
Andrew Bauer <abau171@gmail.com>
2016-03-12 16:24:03 -0500

    moved db build path to application directory

--- src/Test.java
+++ src/Test.java
@@ -8,9 +8,9 @@ import org.h2.tools.DeleteDbFiles;
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		DeleteDbFiles.execute("~", "test", true);
+		DeleteDbFiles.execute(".", "db", true);
 		Class.forName("org.h2.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+		Connection conn = DriverManager.getConnection("jdbc:h2:./db");
 		Statement stat = conn.createStatement();
 		stat.execute("create table test(id int primary key, name varchar(255))");
 		stat.execute("insert into test values(1, 'Hello')");
