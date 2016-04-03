f19ae3bafac5b600e06339a8b79a9f0033a09921
Jordan <jar2119@rit.edu>
2016-04-03 19:41:42 -0400

    Can now add and remove phone numbers when creating customer profile using SignupView.

--- src/edu/rit/cs/Za/SignupView.java
+++ src/edu/rit/cs/Za/SignupView.java
@@ -5,6 +5,11 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -17,30 +22,38 @@ import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerDateModel;
+import javax.swing.JComboBox;
 
 public class SignupView {
 
 	private String[] profileFields= new String[]{
-			"FirstName",
-			"MiddleName",
-			"LastName",
-			"DOB",
-			"Username",
-			"Password",
-			"Address",
-			"City",
-			"State",
-			"Zip",
-			"Email",
-			"Phone"
-		};
+			"First Name:",
+			"Middle Name:",
+			"Last Name:",
+			"Date of Birth:",
+			"Username:",
+			"Password:",
+			"Address:",
+			"City:",
+			"State:",
+			"Zipcode:",
+			"Email:"
+	};
+	
 	private JFrame frame;
 	private JButton signUpButton;
 	private JButton cancelButton;
 	private JPanel profilePanel;
 	private JSpinner DOBSpinner;
 	private JPasswordField pwField;
-	private JScrollPane profileScollPane;
+	//private JScrollPane profileScollPane;
+	
+	JPanel phoneNumberPanel;
+	JComboBox<String> phoneNumberComboBox;
+	JTextField phoneNumberTextField;
+	JButton addPhoneNumberButton;
+	JButton removePhoneNumberButton;
+	JLabel phoneNumberLabel;
 	
 	public void run(){
 		this.frame.setVisible(true);
@@ -52,7 +65,8 @@ public class SignupView {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -64,54 +78,200 @@ public class SignupView {
 			}
 		});
 	}
-	public void initialize(){
+	
+	private void initPhoneNumberPanel()
+	{
+	    phoneNumberPanel = new JPanel(new GridBagLayout());
+	    
+        phoneNumberComboBox = new JComboBox<String>();
+        phoneNumberComboBox.setEnabled(false);
+        
+        phoneNumberTextField = new JTextField();
+        
+        addPhoneNumberButton = new JButton("Add");
+        addPhoneNumberButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt)
+            {
+                String phoneNumber = phoneNumberTextField.getText();
+                phoneNumberTextField.setText("");
+                
+                for (int i = 0; i < phoneNumberComboBox.getItemCount(); ++i)
+                {
+                    String s = phoneNumberComboBox.getItemAt(i);
+                    if (s.equals(phoneNumber)) return;
+                }
+                
+                phoneNumberComboBox.addItem(phoneNumber);
+                phoneNumberComboBox.setEnabled(true);
+            }
+        });
+        
+        removePhoneNumberButton = new JButton("Remove");
+        removePhoneNumberButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt)
+            {
+                if (phoneNumberComboBox.getItemCount() < 1) return;
+                phoneNumberComboBox.removeItemAt(phoneNumberComboBox.getSelectedIndex());
+                if (phoneNumberComboBox.getItemCount() < 1)
+                    phoneNumberComboBox.setEnabled(false);
+            }
+        });
+        
+        phoneNumberLabel = new JLabel("Phone Number:");
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0; gbc.gridy = 0;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.weightx = 0.0; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+        phoneNumberPanel.add(phoneNumberLabel, gbc);
+        
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1; gbc.gridy = 0;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 0.5; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+        phoneNumberPanel.add(phoneNumberTextField, gbc);
+        
+        gbc = new GridBagConstraints();
+        gbc.gridx = 2; gbc.gridy = 0;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.weightx = 0.0; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+        phoneNumberPanel.add(addPhoneNumberButton, gbc);
+        
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1; gbc.gridy = 1;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 0.5; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+        phoneNumberPanel.add(phoneNumberComboBox, gbc);
+        
+        gbc = new GridBagConstraints();
+        gbc.gridx = 2; gbc.gridy = 1;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.weightx = 0.0; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+        phoneNumberPanel.add(removePhoneNumberButton, gbc);
+	}
+	
+	public void initialize()
+	{
+	    initPhoneNumberPanel();
+	    
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);	
-		frame.setSize(700, 500);
+		//frame.setBounds(100, 100, 450, 300);	
+		//frame.setSize(700, 500);
+		frame.setSize(576, 576);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout());
+		//frame.getContentPane().setLayout(new GridBagLayout());
+		//frame.setLayout(new GridBagLayout());
+		JPanel mainPanel = new JPanel(new GridBagLayout());
+		//mainPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
 		profilePanel = new JPanel(new GridBagLayout());
-		profileScollPane = new JScrollPane(profilePanel);
-		JLabel[] arrayLabel = new JLabel[12];
-		JTextField[] arrayTextField = new JTextField[10];
+		//profileScollPane = new JScrollPane(profilePanel);
+		//JLabel[] arrayLabel = new JLabel[profileFields.length];
+		JTextField[] arrayTextField = new JTextField[profileFields.length];
 		SpinnerDateModel model;
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
 		int j = 0;
-		for(int i = 0; i < arrayLabel.length; i++){
-			arrayLabel[i] = new JLabel(profileFields[i]);
-			profilePanel.add(arrayLabel[i],gbc);
-			gbc.gridx++;
-			if(i == 3){
+		for(int i = 0; i < profileFields.length; i++){
+		    
+			//arrayLabel[i] = new JLabel(profileFields[i]);
+		    JLabel attributeLabel = new JLabel(profileFields[i]);
+			GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx = 0; gbc.gridy = i;
+	        gbc.gridwidth = 1; gbc.gridheight = 1;
+	        gbc.fill = GridBagConstraints.NONE;
+	        gbc.anchor = GridBagConstraints.LINE_END;
+	        gbc.weightx = 0.0; gbc.weighty = 1.0;
+	        gbc.ipadx = 2; gbc.ipady = 2;
+	        gbc.insets = new Insets(4, 4, 4, 4);
+			profilePanel.add(attributeLabel, gbc);
+			
+			gbc.gridx = 1;
+            gbc.anchor = GridBagConstraints.LINE_START;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.weightx = 1.0;
+			if(i == 3)
+			{
 				Calendar calendar = Calendar.getInstance();
 				Date currentDate = calendar.getTime();
-				calendar.add(Calendar.YEAR, -100);
+				calendar.add(Calendar.YEAR, -120);
 				Date firstDate = calendar.getTime();
-				calendar.add(Calendar.YEAR, 200);
+				calendar.add(Calendar.YEAR, 120);
+				calendar.add(Calendar.YEAR, -13);
 				Date lastDate = calendar.getTime();
-				model = new SpinnerDateModel(currentDate, firstDate, lastDate, Calendar.YEAR);
+				model = new SpinnerDateModel(lastDate, firstDate, lastDate, Calendar.YEAR);
 				DOBSpinner = new JSpinner(model);
 				DOBSpinner.setEditor(new JSpinner.DateEditor(DOBSpinner, "MM/dd/yyyy"));
 				profilePanel.add(DOBSpinner,gbc);
-			}else if(i == 5){
-				pwField = new JPasswordField(); 
-				profilePanel.add(pwField, gbc);
-			}else{
-				arrayTextField[j] = new JTextField();
-				profilePanel.add(arrayTextField[j],gbc);
-				j++;
 			}
-			gbc.gridy++;
-			gbc.gridx--;
+			else if(i == 5)
+			{
+				pwField = new JPasswordField();
+				profilePanel.add(pwField, gbc);
+			}
+			else
+			{
+				arrayTextField[i] = new JTextField();
+				profilePanel.add(arrayTextField[i],gbc);
+			}
 		}
-		frame.getContentPane().add(profileScollPane, BorderLayout.CENTER);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0; gbc.gridy = 0;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.anchor = GridBagConstraints.PAGE_END;
+        gbc.weightx = 1.0; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+		mainPanel.add(profilePanel, gbc);
+		
+		initPhoneNumberPanel();
+		gbc = new GridBagConstraints();
+        gbc.gridx = 0; gbc.gridy = 1;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 1.0; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+		mainPanel.add(phoneNumberPanel, gbc);
+		
 		signUpButton = new JButton("Submit");
 		cancelButton = new JButton("Cancel");
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(signUpButton);
 		buttonPanel.add(cancelButton);
-		frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		
+		gbc = new GridBagConstraints();
+        gbc.gridx = 0; gbc.gridy = 2;
+        gbc.gridwidth = 1; gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.weightx = 1.0; gbc.weighty = 1.0;
+        gbc.ipadx = 2; gbc.ipady = 2;
+        gbc.insets = new Insets(4, 4, 4, 4);
+		mainPanel.add(buttonPanel, gbc);
+		
+		frame.setLayout(new BorderLayout());
+		frame.add(mainPanel, BorderLayout.CENTER);
+		frame.setResizable(false);
 	}
 }
